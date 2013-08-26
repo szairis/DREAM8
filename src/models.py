650f6b872effbaa57302773019fcbd62f9a886ac
Kevin Emmett <kjemmett@gmail.com>
2013-08-26 01:38:41 -0400

    lasso model

--- src/models.py
+++ src/models.py
@@ -1,12 +1,12 @@
+import pdb
 import os
-from scipy.io import savemat
-from scipy.io import loadmat
 import pandas as pd
 import numpy as np
 from pymatbridge import Matlab
-from sklearn.ensemble import GradientBoostingRegressor
 
-def hill(panel, prior_graph=[], lambdas=[], max_indegree=3, reg_mode='full', stdise=1, silent=0, maxtime=120):
+from utilities import prepare_markov_data, introduce_inhibs, score_network
+
+def network_hill(panel, prior_graph=[], lambdas=[], max_indegree=3, reg_mode='full', stdise=1, silent=0, maxtime=120):
     '''
     run_hill(panel)
 
@@ -15,22 +15,21 @@ def hill(panel, prior_graph=[], lambdas=[], max_indegree=3, reg_mode='full', std
 
     output: dict containing key 'e' and key 'i' from Hill's code
     '''
+    from scipy.io import savemat
+    from scipy.io import loadmat
 
     # start matlab
     mlab = Matlab(maxtime=maxtime)
     mlab.start()
 
-    # slice out the data we want
-    # just one time series for the cantone model
-    D = np.transpose(panel.values, axes=(2,1,0))
-
     # .mat shuttle files
+    # add path check
     inPath = os.path.join('..', 'cache', 'dbn_wrapper_in.mat')
     outPath = os.path.join('..', 'cache', 'dbn_wrapper_out.mat')
 
     # save the matlab object that the DBN wrapper will load
     # contains all the required parameters for the DBN code
-    savemat(inPath, {'D' : D,
+    savemat(inPath, {'D' : panel,
                      'max_indegree' : max_indegree,
                      'prior_graph' : prior_graph,
                      'lambdas' : lambdas,
@@ -53,11 +52,12 @@ def hill(panel, prior_graph=[], lambdas=[], max_indegree=3, reg_mode='full', std
     return (edge_prob, edge_sign)
 
 
-def gbr(n_estimators=300, max_depth=3, learning_rate=0.1):
+def gbr(data, n_estimators=300, max_depth=3, learning_rate=0.1, inhib_targets=None, perfect=True):
     '''
     run gradient boosting regression on design matrix.
     
     Input:
+        data
         design: TxN design matrix. should be a pandas dataframe with each network node
         as a column, and measured time points as rows.
         response: TxN response matrix. should be a pandas dataframe with each network node
@@ -68,13 +68,20 @@ def gbr(n_estimators=300, max_depth=3, learning_rate=0.1):
         Fit object???
     
     '''
+    from sklearn.ensemble import GradientBoostingRegressor
     
+    # model interventions if supplied an inhib_targets dict
+    if inhib_targets:
+        training_dict = prepare_markov_data(introduce_inhibs(data, inhib_targets=inhib_targets, perfect=perfect), response_type, group_stimuli)
+    else:
+        training_dict = prepare_markov_data(data, response_type, group_stimuli)
+
     nodes = design.columns
 
     for target in nodes:
         regGBR[target] = {}
         for stim in stims:
-            y = response[(response['Timepoint']>0) & (response['Stimulus']==stim)]
+            y = response[(response['Timepoint']>0) & (response['Stimulus']==stim)] \
                 .groupby(['Inhibitor', 'Stimulus', 'Timepoint']).mean()[target].values
             
             regGBR[target][stim] = GradientBoostingRegressor(n_estimators=n_estimators,
@@ -89,9 +96,9 @@ def gbr(n_estimators=300, max_depth=3, learning_rate=0.1):
     return 0
 
 
-def network_lasso(data, response_type='level', ground_truth=None, group_stimuli=False):
+def network_lasso(data, response_type='level', ground_truth=None, inhib_targets=None, perfect=True, group_stimuli=False):
     '''
-    lasso
+    do lasso. automatically do CV to find best alpha.
 
     input:
         data
@@ -101,12 +108,38 @@ def network_lasso(data, response_type='level', ground_truth=None, group_stimuli=
     '''
     from sklearn import preprocessing, linear_model, cross_validation, metrics
 
-    training_dict = prepare_markov_data(data, response_type, group_stimuli)
+    # model interventions if supplied an inhib_targets dict
+    if inhib_targets:
+        training_dict = prepare_markov_data(introduce_inhibs(data, inhib_targets=inhib_targets, perfect=perfect), response_type, group_stimuli)
+    else:
+        training_dict = prepare_markov_data(data, response_type, group_stimuli)
 
-    antibodies = data.columns[4:]
+    antibodies = [col for col in data.columns if col not in ['Cell Line', 'Inhibitor', 'Stimuus', 'Timepoint']]
     stims = set(data['Stimulus'])
 
+    # fit lasso for each (X,Y) pair
+    A = {}
+    for key in training_dict:
+        X = training_dict[key][0]
+        Y = training_dict[key][1]
+        preprocessing.StandardScaler().fit_transform(X)
+
+        A[key] = pd.DataFrame(np.zeros((X.shape[1], X.shape[1])), columns=X.columns, index=X.columns)
+
+        for col in Y.columns:
+            print col
+            # check if col is not all the identical
+            if len(set(Y[col])) > 1:
+                rgn = linear_model.LassoCV(verbose=True).fit(X, Y[col])
+                if np.max(rgn.coef_) != 0:
+                    A[key].ix[:,col] = np.abs(rgn.coef_) / np.abs(rgn.coef_).max()
+            else:
+                A[key].ix[:,col] = np.zeros((X.shape[1], 1))
+
     if ground_truth:
-        return A, scatter
+        auc = {}
+        for key in training_dict:
+            auc[key] = score_network(A[key], ground_truth)
+        return A, auc 
     else:
         return A
